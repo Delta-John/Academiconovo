5279dbcb2712483d2ba92f83e85dd02013775785
Delta_John <eric.portela.costa@gmail.com>
2025-11-11 20:27:43 -0300

    academico instituiçao

---
+++ Academico/Models/Instituição.cs
@@ -0,0 +1,9 @@
+﻿namespace Academico.Models
+{
+    public class Instituição
+    {
+        public int InstituiçãoId { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set;}
+    }
+}
